5c3ec6341a6b704bc2fb961d0731018124dfe058
marek <marek@debian.localdomain>
2012-07-02 22:25:36 +0200

    New names for project and app

--- spa/spa/urls.py
+++ spa/spa/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'reserver.views.home', name='home'),
-    # url(r'^reserver/', include('reserver.foo.urls')),
+    # url(r'^$', 'spa.views.home', name='home'),
+    # url(r'^spa/', include('spa.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- spa/spa/wsgi.py
+++ spa/spa/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for reserver project.
+WSGI config for spa project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reserver.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spa.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
