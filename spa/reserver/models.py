e11bb36677b11147703926bb5de2e1a26f6b838c
Marek Brzóska <brzoskamarek@gmail.com>
2012-07-02 23:00:03 +0200

    basic models

--- spa/reserver/models.py
+++ spa/reserver/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Reservable(models.Model):
+    name = models.TextField(null=True, blank=True)
+    owner = models.ForeignKey(User)
+    reservable_group = models.ForeignKey('ReservableGroup', null=True)
+
+
+class ReservableGroup(models.Model):
+    name = models.TextField(null=True, blank=True)
+    owner = models.ForeignKey(User)
+
+
+class Reservation(models.Model):
+    owner = models.ForeignKey(User, null=True)
+    reservable = models.ForeignKey('Reservable') 
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+
+
+
