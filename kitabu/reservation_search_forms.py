26e33073305666e988d9d5ee307f5eee3bf90689
Adrian Duraj <adrian.duraj@gmail.com>
2012-09-17 14:00:38 +0200

    add first forms for searching reservations in subjects and clusters

---
+++ kitabu/reservation_search_forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.forms import ValidationError
+from django.db.models import Q
+
+from kitabu.forms import KitabuSearchForm
+
+
+class BaseReservationSearchForm(KitabuSearchForm):
+    start = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'kitabu-datetime-field'}))
+    end = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'kitabu-datetime-field'}))
+
+    def clean(self):
+        if 'start' in self.cleaned_data and 'end' in self.cleaned_data:
+            start = self.cleaned_data['start']
+            end = self.cleaned_data['end']
+            if not end > start:
+                raise ValidationError('Make sure end date is later than start date')
+        return super(BaseReservationSearchForm, self).clean()
+
+    def search(self, *args, **kwargs):
+        start = self.cleaned_data['start']
+        end = self.cleaned_data['end']
+
+        return self.reservation_model.objects.filter(
+            Q(start__gte=start, start__lt=end) | Q(end__gt=start, end__lte=end) | Q(start__lte=start, end__gte=end),
+            *args, **kwargs
+        )
+
+
+class SingleSubjectReservationSearchMixin(object):
+    '''
+    This class should be mixed in before some descendant of
+    BaseAvailabilityForm as it suplies search method which calls its super
+    '''
+    def search(self, subject, **kwargs):
+        return super(SingleSubjectReservationSearchMixin, self).search(subject=subject, **kwargs)
+
+
+class SingleClusterReservationSearchMixin(object):
+    '''
+    This class should be mixed in before some descendant of
+    BaseAvailabilityForm as it suplies search method which calls its super
+    '''
+    def search(self, subject_model_manager, **kwargs):
+        return super(SingleClusterReservationSearchMixin, self).search(subject__in=subject_model_manager.all(),
+                                                                       **kwargs)
--- pools/forms.py
+++ pools/forms.py
@@ -1,9 +1,14 @@
 from kitabu.availability_forms import FiniteAvailabilityForm, OneClusterAvailabilityFormMixin
+from kitabu.reservation_search_forms import SingleClusterReservationSearchMixin, BaseReservationSearchForm
 
-from lanes.models import Lane
+from lanes.models import Lane, LaneReservation
 
 
 class AvailableLanesSearchForm(
         OneClusterAvailabilityFormMixin,
         FiniteAvailabilityForm.on_model(Lane)):
     pass
+
+
+class PoolReservationSearchForm(SingleClusterReservationSearchMixin, BaseReservationSearchForm):
+    reservation_model = LaneReservation
