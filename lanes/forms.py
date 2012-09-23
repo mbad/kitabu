17744ddd50239b86100cc679bbadc473d8a2432b
Marek Brzóska <brzoskamarek@gmail.com>
2012-09-23 17:55:29 +0200

    Remove buggy validation

--- lanes/forms.py
+++ lanes/forms.py
@@ -1,6 +1,5 @@
 from kitabu.forms.reservation import ReservationWithSizeForm
 from kitabu.forms.availability import ExclusiveAvailabilityForm
-from kitabu.forms.validators import FullHourValidator, FullMinuteValidator
 
 from models import LaneReservation, Lane
 
@@ -11,14 +10,6 @@ class LaneReservationForm(ReservationWithSizeForm, PostForm):
     reservation_model = LaneReservation
 
     submit_button_text = 'Reserve'
-
-    def clean_start(self):
-        if 'start' in self.cleaned_data:
-            FullHourValidator(message='Only full hours allowed')(self.cleaned_data['start'])
-
-    def clean_end(self):
-        if 'end' in self.cleaned_data:
-            FullMinuteValidator(message='Only full minutes allowed')(self.cleaned_data['end'])
 
 
 class AvailableLanesSearchForm(ExclusiveAvailabilityForm.on_model(Lane), SearchForm):
