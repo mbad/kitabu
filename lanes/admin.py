62f0541f173b31e3e4e877ea097814afb2d09b63
Marek Brzóska <brzoskamarek@gmail.com>
2013-02-09 19:34:34 +0100

    enhance admin for validators

--- lanes/admin.py
+++ lanes/admin.py
@@ -10,6 +10,18 @@ admin.site.register(models.Lane, LaneAdmin)
 admin.site.register(models.LaneReservation)
 
 admin.site.register(models.LaneFullTimeValidator)
+admin.site.register(models.LaneGivenHoursAndWeekdaysValidator)
 admin.site.register(models.LaneTimeIntervalValidator)
 admin.site.register(models.LaneNotWithinPeriodValidator)
-admin.site.register(models.LaneWithinPeriodValidator)
+admin.site.register(models.LaneMaxReservationsPerUserValidator)
+
+
+class PeriodInline(admin.TabularInline):
+    model = models.Period
+
+
+class WithinPeriodValidatorAdmin(admin.ModelAdmin):
+    inlines = [PeriodInline]
+
+
+admin.site.register(models.LaneWithinPeriodValidator, WithinPeriodValidatorAdmin)
