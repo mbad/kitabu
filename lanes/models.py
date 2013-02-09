560a781f38b5fee995633e303335a7048ec8f488
Marek Brzóska <brzoskamarek@gmail.com>
2013-02-09 18:24:04 +0100

    fix spa to work with mixin version of Reservations/Subjects

--- lanes/models.py
+++ lanes/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from kitabu.models.subjects import VariableSizeSubject
-from kitabu.models.reservations import ReservationMaybeExclusive, ReservationGroup
+from kitabu.models.subjects import VariableSizeSubject, BaseSubject
+from kitabu.models.reservations import ReservationMaybeExclusive, ReservationGroup, BaseReservation
 from kitabu.models import validators
 
 from pools.models import Pool
 
 
-class Lane(VariableSizeSubject):
+class Lane(VariableSizeSubject, BaseSubject):
     name = models.TextField()
     cluster = models.ForeignKey(Pool, related_name='subjects')
 
@@ -16,7 +16,7 @@ class Lane(VariableSizeSubject):
         return self.name
 
 
-class LaneReservation(ReservationMaybeExclusive):
+class LaneReservation(ReservationMaybeExclusive, BaseReservation):
     subject = models.ForeignKey('Lane', related_name='reservations')
     group = models.ForeignKey('LaneReservationGroup', related_name='reservations', null=True, blank=True)
     owner = models.ForeignKey(User, null=True)
