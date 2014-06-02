fb7ba969a3d7b94c46db8b99c827b8354ff6c991
Adrian Duraj <adrian.duraj@gmail.com>
2014-06-02 23:23:20 +0200

    fix data loader

--- spa/management/commands/load_data.py
+++ spa/management/commands/load_data.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand
-from lanes.models import Lane
+from lanes.models import Lane, LaneFullTimeValidator, LaneGivenHoursAndWeekdaysValidator
 from pools.models import Pool
 
 
@@ -15,21 +15,34 @@ class Command(BaseCommand):
 class DataLoader(object):
     def __init__(self):
         self.pools = {}
+        self.validators = {}
 
     def load_all(self):
         self._load_pools()
+        self._load_validators()
         self._load_lanes()
 
     def _load_pools(self):
         for pool_name in ['Konopnicka', 'Cygański', 'Aqua', 'Dmuchany przy domu', 'Olimpijski (Pekin)']:
             try:
                 pool = Pool.objects.get(name=pool_name)
+                for lane in pool.lanes.all():
+                    lane.validators.all().delete()
                 pool.delete()
             except Pool.DoesNotExist:
                 pass
             self.pools[pool_name] = Pool.objects.create(name=pool_name)
 
+    def _load_validators(self):
+        self.validators['Konopnicka'] = LaneFullTimeValidator.objects.create(interval=1, interval_type='hour')
+
+        # Monday to Friday
+        self.validators['Cygański'] = LaneGivenHoursAndWeekdaysValidator.create_from_bitlists(
+            hours=[1] * 24, days=[1, 1, 1, 1, 1, 0, 0])
+
     def _load_lanes(self):
         for pool in self.pools.values():
             for i in range(1, 11):
-                Lane.objects.create(name="Tor {0} - {1}".format(i, pool.name), cluster=pool, size=i + 10)
+                lane = Lane.objects.create(name="Tor {0} - {1}".format(i, pool.name), cluster=pool, size=i + 10)
+                if pool.name in self.validators:
+                    lane.validators.add(self.validators[pool.name])
